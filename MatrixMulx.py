be53e80f7e8ab44fd3a5ac4a480996eae15d4ae9
Kartik Hans <21987980+kartikhans@users.noreply.github.com>
2024-05-02 21:41:39 +0530

    Cpu intensive workloads (#1)
    * Simple matrix mulx and mapReduce Sort added
    
    * add: IO intensive job added, without .env and key.json file
    
    * add: keyPath moved to .env
    
    ---------
    
    Co-authored-by: Kush Hans <kushhans@kushs-air.mynetworksettings.com>
    Co-authored-by: Kush Hans <kushhans@Kushs-MacBook-Air.local>
    Co-authored-by: Kush Hans <kushhans@mc-wifi-10-215-120-92.wireless.pitt.edu>

---
+++ MatrixMulx.py
@@ -0,0 +1,47 @@
+import random
+import time
+
+
+def generate_random_number():
+    return random.randint(1, 100)
+
+
+class MatrixMulx:
+    def __init__(self, n):
+        self.matrixB = None
+        self.matrixA = None
+        self.n = n
+        self.result = None
+
+    def create_matrix(self):
+        self.matrixA = [[0 for i in range(self.n)] for j in range(self.n)]
+        for i in range(self.n):
+            for j in range(self.n):
+                self.matrixA[i][j] = generate_random_number()
+
+        self.matrixB = [[0 for i in range(self.n)] for j in range(self.n)]
+        for i in range(self.n):
+            for j in range(self.n):
+                self.matrixB[i][j] = generate_random_number()
+
+        self.result = [[0 for i in range(self.n)] for j in range(self.n)]
+
+    def multiply(self):
+        # iterate through rows of X
+        self.create_matrix()
+        for i in range(self.n):
+            # iterate through columns of Y
+            for j in range(self.n):
+                # iterate through rows of Y
+                for k in range(self.n):
+                    self.result[i][j] += self.matrixA[i][k] * self.matrixB[k][j]
+
+    def execute(self):
+        start_time = time.time()
+        self.multiply()
+        print(time.time() - start_time)
+
+if __name__ == '__main__':
+    instance = MatrixMulx(800)
+    instance.execute()
+

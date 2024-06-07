e2ee0bb99c93ea6a407d8ce0e3d723aa5660fa2b
Kush Hans <kushhans@Kushs-MacBook-Air.local>
2024-06-07 14:26:30 -0400

    add: constant file added for storing carbon emission of different systems

--- SaveInEmission.py
+++ SaveInEmission.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
+from constant import EMISSIONS
 import multiprocessing
+
 
 class SaveInEmission:
     def __init__(self, embodied_em_old, energy_avg_old, energy_avg_new, expected_age, emission_per_unit):
@@ -10,23 +12,23 @@ class SaveInEmission:
         self.expectedAge = expected_age * 365 * 24 * 60 * 60
 
     def saved_emission(self, old_time, new_time):
-        saved_by_embodied = (self.emNew/self.expectedAge)*new_time
+        saved_by_embodied = (self.emNew / self.expectedAge) * new_time
 
-        saved_by_energy = (self.avgEnergyNew*new_time - self.avgEnergyOld*old_time)*self.emissionPerUnit
+        saved_by_energy = (self.avgEnergyNew * new_time - self.avgEnergyOld * old_time) * self.emissionPerUnit
 
         return saved_by_energy + saved_by_embodied
 
     def old_emission_range(self, new_time):
-        saved_by_embodied = (self.emNew/(self.expectedAge*self.emissionPerUnit))
+        saved_by_embodied = (self.emNew / (self.expectedAge * self.emissionPerUnit))
 
         saved_by_energy = self.avgEnergyNew
 
-        return f"Total Energy of the device should be strictly less than {new_time * (saved_by_energy + 
+        return f"Total Energy of the device should be strictly less than {new_time * (saved_by_energy +
                                                                                       saved_by_embodied)}"
 
 
-
 if __name__ == '__main__':
+
     print("")
 
     m = multiprocessing.Manager()
---
+++ constant.py
@@ -0,0 +1,5 @@
+EMISSIONS = {
+    "M1": 181,
+    "M2": 147,
+    "UNIT": "kg Co2e"
+}
